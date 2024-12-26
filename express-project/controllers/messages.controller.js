99581eee28dd1ab053e5d6e79aca4de3b0852734
kouki-16 <kouki-16@og.com>
2024-12-26 16:01:00 +0900

    express

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,6 +1,11 @@
 
+const path = require('path');
+
+
 function getMessages(req, res) {
-    res.send('hello this is Node js Message');
+    res.sendFile(path.join(__dirname, '..', 'test.jpg'));
+    //res.sendFile('test.jpg');
+    //res.send('hello this is Node js Message');
 }
 
 function postMessage(req, res) {
--- express-project/server.js
+++ express-project/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter   = require('./router/friends.router');
 const messageRouter   = require('./router/messages.router');
 
 const app = express();
 const port = 3000;
+
 
 
 app.use((req, res, next) => {
@@ -15,7 +17,25 @@ app.use((req, res, next) => {
     console.log(`${req.url}${req.url} ${delta}ms`);
 });
 
+// REST API  == Representational State Transfer
 app.use(express.json());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        caption: "this is test page",
+    });
+});
+
+app.get('/messages', (req, res) => {
+    res.render('messages', {
+        friend: "Albart",
+    });
+});
+
+
 app.use('/friends', friendsRouter);
 app.use('/messages', messageRouter);
 
