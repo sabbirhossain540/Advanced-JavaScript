13046561adfdd11756958ccec6a09a2fc36bb450
kouki-16 <kouki-16@og.com>
2025-01-06 16:56:11 +0900

    d

--- nasa_project/client/src/hooks/requests.js
+++ nasa_project/client/src/hooks/requests.js
@@ -1,6 +1,7 @@
+
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch('http://localhost:8080/planets');
+  return await response.json();
 }
 
 async function httpGetLaunches() {
